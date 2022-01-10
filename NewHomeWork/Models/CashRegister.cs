556912ead404b5ee52c97eae0f995841608ba6cd
Mahammad2255 <[mahammadei@code.edu.az]>
2022-01-10 12:29:43 -0800

    Add project files.

---
+++ NewHomeWork/Models/CashRegister.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHomeWork.Models
+{
+    class CashRegister
+    {
+        public double TotalAmount { get; set; }
+        public Currency Currency { get; set; }
+        public int TotalSalesCount;
+        public PaymentType PaymentType { get; set; }
+        public CashRegister(double totalamount)
+        {
+
+            TotalAmount = totalamount;
+
+        }
+        public void AddNewSale(double value, Currency currency, PaymentType paymentType)
+        {
+            TotalSalesCount++;
+            switch (PaymentType)
+            {
+                case PaymentType.Kart:
+                    Console.WriteLine("Kartla odenis olundu");
+                    break;
+                case PaymentType.Nagd:
+                    Console.WriteLine("Nagd odenis olundu");
+                    break;
+                default:
+                    break;
+            }
+            switch (Currency)
+            {
+                case Currency.Azn:
+                    TotalAmount += value;
+                    break;
+                case Currency.Sterling:
+                    TotalAmount += value * 2;
+                    break;
+                case Currency.Dollar:
+                    TotalAmount += value * 1.7;
+                    break;
+                default:
+
+                    break;
+            }
+        }
+        public void RemoveSale(double value, Currency currency)
+        {
+
+
+
+            switch (Currency)
+            {
+                case Currency.Azn:
+                    TotalAmount -= value;
+                    break;
+                case Currency.Sterling:
+                    TotalAmount -= value * 2;
+                    break;
+                case Currency.Dollar:
+                    TotalAmount -= value * 1.7;
+                    break;
+                default:
+
+                    break;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $" {TotalAmount}{TotalSalesCount}";
+
+        }
+    }
+}
---
+++ NewHomeWork/Program.cs
@@ -0,0 +1,28 @@
+﻿using NewHomeWork.Models;
+using System;
+
+namespace NewHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister cashRegister = new CashRegister(1000);
+            cashRegister.AddNewSale(100, Currency.Azn, PaymentType.Kart);
+            Console.WriteLine(cashRegister.TotalAmount);
+            cashRegister.AddNewSale(42, Currency.Azn, PaymentType.Nagd);
+            Console.WriteLine(cashRegister.TotalAmount);
+            cashRegister.AddNewSale(37, Currency.Sterling, PaymentType.Kart);
+            Console.WriteLine(cashRegister.TotalAmount);
+            cashRegister.AddNewSale(100, Currency.Dollar, PaymentType.Nagd);
+            Console.WriteLine(cashRegister.TotalAmount);
+            cashRegister.AddNewSale(200, Currency.Azn, PaymentType.Kart);
+            Console.WriteLine(cashRegister.TotalAmount);
+            Console.Write("Satislar silindikden sonra son netice:");
+            cashRegister.RemoveSale(100, Currency.Azn);
+            Console.WriteLine(cashRegister.TotalAmount);
+            Console.Write("Emeliyyatlarin sayi:");
+            Console.WriteLine(cashRegister.TotalSalesCount);
+        }
+    }
+}
